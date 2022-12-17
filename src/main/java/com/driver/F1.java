61ece2dd660cf5f24b6049e449c4ace693f450c3
Ravi Kishan Raj <ravikishan624@.com>
2022-12-17 17:28:32 +0530

    done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -20,18 +20,26 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed >= 0 && newSpeed <= 50) {
+        if(newSpeed == 0) {
             //Stop the car, set gear as 1
+
             changeGear(1);
-        } else if (newSpeed >= 51 && newSpeed <= 100) {
+        } else if (newSpeed >= 0 && newSpeed<=50) {
+            changeGear(1);
+        }
+        else if (newSpeed >= 51 && newSpeed<=100) {
             changeGear(2);
-        }else if (newSpeed >= 101 && newSpeed <= 150) {
+        }
+        else if (newSpeed >= 101 && newSpeed<=150) {
             changeGear(3);
-        }else if (newSpeed >= 151 && newSpeed <= 200) {
+        }
+        else if (newSpeed >=151 && newSpeed<=200) {
             changeGear(4);
-        }else if (newSpeed >= 201 && newSpeed <= 250) {
+        }
+        else if (newSpeed >= 201 && newSpeed<=250) {
             changeGear(5);
-        }else if(newSpeed > 250){
+        }
+        else if((newSpeed>250)){
             changeGear(6);
         }
         //for all other cases, change the gear accordingly
