fe127e4a69079eb1834e7acc97ce2110b142b366
Ravi Kishan Raj <ravikishan624@.com>
2022-12-17 19:44:43 +0530

    done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -9,8 +9,8 @@ public class F1 extends Car {
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        this.setCurrentSpeed(rate);
         newSpeed = rate + this.getCurrentSpeed();
+//        this.setCurrentSpeed(rate);
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
